bb8ffe5d5a930b43f4ca6107f922f70d8f8ad14c
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-18 21:03:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
   if (board[index] == " " || board[index] == "" || board[index] = nil)
-    return false
+    return "false"
   else
-    return true
+    return "true"
   end
 end
